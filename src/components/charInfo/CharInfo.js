39657b764d367cbfa3683d0ecf49b011423d9add
Damakas <tather13@gmail.com>
2024-08-27 13:20:28 +0300

    PropTypes

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -3,6 +3,7 @@ import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton';
 import { Component } from 'react';
+import PropTypes from 'prop-types'
 import MarvelService from '../../services/MarvelService';
 
 class CharInfo extends Component {
@@ -119,6 +120,10 @@ const View = ({ char }) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.string
 }
 
 export default CharInfo;
